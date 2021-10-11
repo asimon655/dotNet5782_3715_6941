dcc9937374ca2937a6b8f2870720297b0d0ff6d0
idanmaman1 <79476119+idanmaman1@users.noreply.github.com>
2021-10-11 12:16:33 +0300

    intial code + Welcome function B

---
+++ dotNet5782_3715_6941/Targil0/Program6941.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    class Program6941
+    {
+        static partial  void Welcome6941()
+        {
+            Console.WriteLine("hello i am also here ");
+        } 
+    }
+}
