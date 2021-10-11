b82f0aaa046e309d87869003218ec41e5bb582f8
idanmaman1 <79476119+idanmaman1@users.noreply.github.com>
2021-10-11 13:09:57 +0300

    enum

---
+++ dotNet5782_3715_6941/DAL/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+namespace IDAL
+{
+    namespace DO
+    {
+        enum WeightCategories
+        {
+            [Description("easy weigth ")]
+            easy =0 ,
+            [Description("medium weigth ")]
+            medium = 0,
+            [Description("heavy weigth ")]
+            heavy = 0,
+
+
+        };
+    
+    
+    } 
+}
