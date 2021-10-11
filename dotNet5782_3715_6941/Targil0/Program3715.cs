fdd91ba1941084cf5d1b75724d63426791cfb548
asimon655 <avrahams655@gmail.com>
2021-10-11 12:11:01 +0300

    inital code + Welcome functions

---
+++ dotNet5782_3715_6941/Targil0/Program3715.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Wellcome3715();
+        }
+
+        static partial void Wellcome6941();
+        private static void Wellcome3715()
+        {
+            Console.WriteLine("Enter your name: ");
+            Console.WriteLine("{0}, Welcome to my first console application", Console.ReadLine());
+        }
+    }
+}
