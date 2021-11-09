0275a8c8a26d51a0c3f170dd4f0d01d2f569ba1b
asimon655 <avrahams655@gmail.com>
2021-11-09 16:24:08 +0000

    Priorities and WeightCategories need to be in DAL

--- dotNet5782_3715_6941/DAL/Enums.cs
+++ dotNet5782_3715_6941/DAL/Enums.cs
@@ -29,5 +29,24 @@ namespace IDAL
 {
     namespace DO
     {
+        public enum Priorities
+        {
+            [Description("Regular delivery ")]
+            Regular,
+            [Description("fast delivery")]
+            Fast,
+            [Description("Emergency delivery ")]
+            Emergency
+    ,
+        }
+        public enum WeightCategories
+        {
+            [Description("easy weight ")]
+            Easy,
+            [Description("medium weight ")]
+            Medium,
+            [Description("heavy weight  ")]
+            Heavy,
+        };
     }
 }
