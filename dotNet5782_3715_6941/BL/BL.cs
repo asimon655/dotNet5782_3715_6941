d7d3188401f63d9e2e85009e0613a0adedf925cf
asimon655 <avrahams655@gmail.com>
2021-11-10 16:17:43 +0200

    constructor

--- dotNet5782_3715_6941/BL/BL.cs
+++ dotNet5782_3715_6941/BL/BL.cs
@@ -3,8 +3,13 @@
 namespace BL
 {
     public class Bl : IBL.Ibl
-    { 
-    
-    
+    {
+        IDAL.Idal data = new DAL.DalObject.DalObject();
+
+        public Bl()
+        {
+
+        }
+
     } 
 }
