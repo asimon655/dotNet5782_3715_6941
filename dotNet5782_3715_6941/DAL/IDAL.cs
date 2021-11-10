88829ee56fcdc566a53e7338a9c2fbe979dbcadf
asimon655 <avrahams655@gmail.com>
2021-11-10 16:24:14 +0200

    add GetPowerConsumption to interface IDAL

--- dotNet5782_3715_6941/DAL/IDAL.cs
+++ dotNet5782_3715_6941/DAL/IDAL.cs
@@ -8,22 +8,22 @@ namespace IDAL
     public interface Idal
     {
         void AddCostumer(Costumer costumer);
-        Costumer PullDataCostumer(int id);
+        Costumer? PullDataCostumer(int id);
         IEnumerable<Costumer> CostumersPrint();
         void UpdateCostumers(Costumer costumer);
 
         void AddDrone(Drone drone);
-        Drone PullDataDrone(int id);
+        Drone? PullDataDrone(int id);
         IEnumerable<Drone> DronesPrint();
         void UpdateDrones(Drone drone);
 
-        void AddStation(Station station);
-        Station PullDataStation(int id);
-        IEnumerable<Station> StationsPrint();
-        void UpdateStations(Station station);
+        void AddStaion(Station station);
+        Station? PullDataStation(int id);
+        IEnumerable<Station> StaionsPrint();
+        void UpdateStaions(Station station);
 
         void AddParcel(Parcel parcel);
-        Parcel PullDataParcel(int id);
+        Parcel? PullDataParcel(int id);
         IEnumerable<Parcel> ParcelsPrint();
         void UpdateParcles(Parcel parcel);
 
