f0e2b9d35b536a755c785411205ae0f6f685e6ac
Avraham simon <avrahams655@gmail.com>
2021-11-16 19:06:39 +0200

    smarter Initalize for data

--- dotNet5782_3715_6941/DAL/DalObject.cs
+++ dotNet5782_3715_6941/DAL/DalObject.cs
@@ -41,10 +41,10 @@ namespace DAL
             //until here array var declartion 
             static internal class Config
             {
-                static internal double PowerConsumptionFree = 3;
-                static internal double PowerConsumptionLight = 6;
-                static internal double PowerConsumptionMedium = 8;
-                static internal double PowerConsumptionHeavy = 10;
+                static internal double PowerConsumptionFree = 0.03;
+                static internal double PowerConsumptionLight = 0.06;
+                static internal double PowerConsumptionMedium = 0.08;
+                static internal double PowerConsumptionHeavy = 0.01;
                 static internal double ChargingSpeed = 3;
 
                 static internal int IdCreation = 0;
@@ -78,31 +78,62 @@ namespace DAL
                     for (int i = 0; i < DroneInit; i++)
                         Drones.Add( new Drone() {
                             Id = RandomGen.Next(100000000, 999999999),
-                            Modle = " V-Coptr Falcon",
+                            Modle = "V-Coptr Falcon",
                             MaxWeigth = (WeightCategories)RandomGen.Next(0, 2)
                         });
                     for (int i = 0; i < ParcelInit; i++)
                     {
                         DateTime scheduledtmp = new DateTime(1995, 1, 1).AddSeconds(RandomGen.Next(0, 86400)).AddDays(RandomGen.Next((DateTime.Today - new DateTime(1995, 1, 1)).Days));
                         /// created random time between today today to 1955 1th in janury 12:00:00 AM 
-                        DateTime requested =scheduledtmp.AddSeconds(RandomGen.Next(0, 86400*365));/// 86400 is one day in secs 
-                                                                                                  /// addeed time to the randomed time i created before 
-                        DateTime pickedup = requested.AddSeconds(RandomGen.Next(0, 86400 * 365));/// 86400 is one day in secs 
-                                                                                                  /// addeed time to the randomed added  time i created before 
-                        DateTime delivered = pickedup.AddSeconds(RandomGen.Next(0, 86400 * 365));/// 86400 is one day in secs 
-                                                                                                 /// addeed time to the randomed added  added time i created before 
-                        Parcels.Add ( new Parcel() {
+                        Parcel parcel = new Parcel() {
                             Id = ++IdCreation,
                             Priority = (Priorities)RandomGen.Next(0, 2),
                             Weight = (WeightCategories)RandomGen.Next(0, 2),
-                            DroneId = RandomGen.Next(100000000, 999999999),
-                            Delivered = delivered,
-                            PickedUp = pickedup,
-                            Requested = requested,
                             Schedulded = scheduledtmp,
-                            SenderId = RandomGen.Next(100000000, 999999999),
-                            TargetId = RandomGen.Next(100000000, 999999999)
-                        });
+                            SenderId = Costumers[RandomGen.Next(Costumers.Count)].Id,
+                            TargetId = Costumers[RandomGen.Next(Costumers.Count)].Id
+                        };
+                        switch (i%4)
+                        {
+                            case (0):
+                            {
+                                DateTime requested =scheduledtmp.AddSeconds(RandomGen.Next(0, 86400*365));/// 86400 is one day in secs 
+                                                                                                        /// addeed time to the randomed time i created before 
+                                parcel.Requested = requested;
+                                parcel.DroneId = Drones[RandomGen.Next(Drones.Count)].Id;
+                                DateTime pickedup = requested.AddSeconds(RandomGen.Next(0, 86400 * 365));/// 86400 is one day in secs 
+                                                                                                        /// addeed time to the randomed added  time i created before 
+                                parcel.PickedUp = pickedup;
+                                DateTime delivered = pickedup.AddSeconds(RandomGen.Next(0, 86400 * 365));/// 86400 is one day in secs 
+                                                                                                        /// addeed time to the randomed added  added time i created before 
+                                parcel.Delivered = delivered;
+                            }
+                            break;
+                            case (1):
+                            {
+                                DateTime requested =scheduledtmp.AddSeconds(RandomGen.Next(0, 86400*365));/// 86400 is one day in secs 
+                                                                                                        /// addeed time to the randomed time i created before 
+                                parcel.Requested = requested;
+                                parcel.DroneId = Drones[RandomGen.Next(Drones.Count)].Id;
+                                DateTime pickedup = requested.AddSeconds(RandomGen.Next(0, 86400 * 365));/// 86400 is one day in secs 
+                                                                                                        /// addeed time to the randomed added  time i created before 
+                                parcel.PickedUp = pickedup;
+                            }
+                            break;
+                            case (2):
+                            {
+                                DateTime requested =scheduledtmp.AddSeconds(RandomGen.Next(0, 86400*365));/// 86400 is one day in secs 
+                                                                                                        /// addeed time to the randomed time i created before 
+                                parcel.Requested = requested;
+                                parcel.DroneId = Drones[RandomGen.Next(Drones.Count)].Id;
+                            }
+                            break;
+                            case (3):
+                            // not even binded
+                            break;
+                        }
+                        
+                        Parcels.Add (parcel);
                     } 
                 }
 
