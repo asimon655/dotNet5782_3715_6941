8a7701a8e50fffae190113cc1f759df4f19371a4
Avraham simon <avrahams655@gmail.com>
2022-01-06 19:09:05 +0200

    orgenizing the convertors

--- dotNet5782_3715_6941/BL/BL/Convertor.cs
+++ dotNet5782_3715_6941/BL/BL/Convertor.cs
@@ -5,51 +5,8 @@ namespace BL
 {
     public sealed partial class Bl : BlApi.Ibl
     {
-         private CustomerList CltToLstC(DO.Customer gety) =>  new CustomerList() { 
-                Id = gety.Id 
-                ,Name=gety.Name 
-                ,Phone=gety.Phone 
-                ,ParcelDeliveredAndGot =data.CountParcels(x => x.SenderId == gety.Id && ParcelStatusC(x) == ParcelStatus.Delivered)  
-                ,InTheWay= data.CountParcels(x => x.SenderId == gety.Id && ParcelStatusC(x) != ParcelStatus.Delivered)
-                ,ParcelGot = data.CountParcels(x => x.TargetId == gety.Id && ParcelStatusC(x) == ParcelStatus.Delivered) 
-                ,ParcelDeliveredAndNotGot = data.CountParcels(x => x.SenderId == gety.Id && ParcelStatusC(x) != ParcelStatus.Delivered) };
-
-        private ParcelStatus ParcelStatusC(DO.Parcel parcel)
-        {
-
-            int caseNum = -1;
-            if (!(parcel.Schedulded is null))
-            {
-                caseNum++;
-                if (!(parcel.Requested is null))
-                {
-                    caseNum++;
-                    if (!(parcel.PickedUp is null))
-                    {
-                        caseNum++; 
-                        if (!(parcel.Delivered is null))
-                        {
-                            caseNum++;
-                        }
-                    }
-                }
-            }
-            if (caseNum == -1)
-                throw new EnumOutOfRange("the parcel is not even decleared ",-1); 
-            return (ParcelStatus)caseNum;
-        }
-        
-        private ParcelInCustomer CustomerToParcelC(DO.Parcel parcel, CustomerInParcel parentCustomer)
-        {
-            return new ParcelInCustomer() {
-                Id = parcel.Id,
-                Priority = (Priorities)parcel.Priority,
-                Status = ParcelStatusC(parcel),
-                Weight = (WeightCategories)parcel.Weight,
-                ParentCustomer = parentCustomer
-            };
-        }
-        private Drone DronesC(DroneList drone)
+        #region Convert
+        private Drone Convert(DroneList drone)
         {
             Drone newDrone = new Drone(){
                 Id = drone.Id,
@@ -86,8 +43,7 @@ namespace BL
             }
             return newDrone;
         }
-
-        private Station StationC(DO.Station station) => new Station(){
+        private Station Convert(DO.Station station) => new Station(){
                 Id = station.Id,
                 NumOfFreeOnes = station.ChargeSlots,
                 LoctConstant = new Location(station.Longitude, station.Lattitude),
@@ -96,9 +52,7 @@ namespace BL
                     DroneId = drones.DroneId ,
                     Battery = GetDroneToList(drones.DroneId).Battery }  ) ).ToList ()  
                 };
-
-
-        private Customer CostumerC(DO.Customer costumer) => new Customer() { 
+        private Customer Convert(DO.Customer costumer) => new Customer() { 
             Id = costumer.Id, 
             Loct = new Location(costumer.Longitude, costumer.Lattitude), 
             Name = costumer.Name, 
@@ -106,9 +60,7 @@ namespace BL
             FromClient = ( from package in data.GetParcels(x => x.SenderId == costumer.Id)  select (CustomerToParcelC(package, new CustomerInParcel() {id = costumer.Id, name = costumer.Name})) ).ToList() ,
             ToClient = (from package in data.GetParcels(x => x.TargetId == costumer.Id) select (CustomerToParcelC(package, new CustomerInParcel() {id = costumer.Id, name = costumer.Name}))).ToList()
         };
-
-
-        private Parcel ParcelC(DO.Parcel parcel)
+        private Parcel Convert(DO.Parcel parcel)
         {
             try
             {
@@ -139,6 +91,69 @@ namespace BL
                 throw new IdDosntExists(err); 
             } 
         }
+        #endregion
 
+        #region ConvertList
+        private CustomerList ConvertList(DO.Customer gety) =>  new CustomerList() { 
+                Id = gety.Id 
+                ,Name=gety.Name 
+                ,Phone=gety.Phone 
+                ,ParcelDeliveredAndGot =data.CountParcels(x => x.SenderId == gety.Id && ParcelStatusC(x) == ParcelStatus.Delivered)  
+                ,InTheWay= data.CountParcels(x => x.SenderId == gety.Id && ParcelStatusC(x) != ParcelStatus.Delivered)
+                ,ParcelGot = data.CountParcels(x => x.TargetId == gety.Id && ParcelStatusC(x) == ParcelStatus.Delivered) 
+                ,ParcelDeliveredAndNotGot = data.CountParcels(x => x.SenderId == gety.Id && ParcelStatusC(x) != ParcelStatus.Delivered) };
+        private ParcelList ConvertList(DO.Parcel x) => new ParcelList()
+        {
+            Id = x.Id,
+            ParcelStatus = ParcelStatusC(x),
+            Priorety = (Priorities)x.Priority,
+            SenderName = data.GetCustomer(x.SenderId).Name,
+            TargetName = data.GetCustomer(x.TargetId).Name,
+            Weight = (WeightCategories)x.Weight
+        };
+        private StationList ConvertList(DO.Station station) => new StationList() { 
+                Id = station.Id,
+                Name = station.Name,
+                FreePorts = station.ChargeSlots,
+                BusyPorts = data.CountDronesCharges(x => x.StaionId == station.Id)
+            };
+        #endregion
+        
+        #region other Converts
+        private ParcelStatus ParcelStatusC(DO.Parcel parcel)
+        {
+
+            int caseNum = -1;
+            if (!(parcel.Schedulded is null))
+            {
+                caseNum++;
+                if (!(parcel.Requested is null))
+                {
+                    caseNum++;
+                    if (!(parcel.PickedUp is null))
+                    {
+                        caseNum++; 
+                        if (!(parcel.Delivered is null))
+                        {
+                            caseNum++;
+                        }
+                    }
+                }
+            }
+            if (caseNum == -1)
+                throw new EnumOutOfRange("the parcel is not even decleared ",-1); 
+            return (ParcelStatus)caseNum;
+        }
+        private ParcelInCustomer CustomerToParcelC(DO.Parcel parcel, CustomerInParcel parentCustomer)
+        {
+            return new ParcelInCustomer() {
+                Id = parcel.Id,
+                Priority = (Priorities)parcel.Priority,
+                Status = ParcelStatusC(parcel),
+                Weight = (WeightCategories)parcel.Weight,
+                ParentCustomer = parentCustomer
+            };
+        }
+        #endregion
     }
 } 
